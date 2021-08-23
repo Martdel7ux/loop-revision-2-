980e6ab7b634ea4b92994a3a2d21f03b93d68f73
Martin Ahoto <martinahoto4@gmail.com>
2021-08-23 19:38:32 +0300

    Initial Commit

---
+++ loop(revision 2)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  loop(revision 2)
+//
+//  Created by MARTIN on 23/08/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

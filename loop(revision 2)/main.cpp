2b781c7af0ff6c30ecd8da5a1cf51a03001f0263
Martin Ahoto <martinahoto4@gmail.com>
2021-08-23 19:51:58 +0300

    second loop basic problem.

--- loop(revision 2)/main.cpp
+++ loop(revision 2)/main.cpp
@@ -1,14 +1,34 @@
 //
 //  main.cpp
 //  loop(revision 2)
-//
+// Question(calculate the class average of the grade of students)
 //  Created by MARTIN on 23/08/2021.
 //
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    int total = 0;
+    int grade;
+    int average;
+    int count=1;
+    
+     
+    
+    while( count<=10)
+    {
+        cout<<"enter a grade"<<endl;
+        cin>>grade;
+        
+        total=total+grade;
+        count++;
+    }
+    
+    average=total/10;
+    
+    cout<<"class average ="<<average;
+    
     return 0;
 }
